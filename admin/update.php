d9d44664e568ea2c7908f54aacac1d4ced445f82
abhisheksaurabh7785 <72158831+abhisheksaurabh7785@users.noreply.github.com>
2020-10-20 09:54:07 +0530

    Update update.php

--- admin/update.php
+++ admin/update.php
@@ -3,47 +3,24 @@ include ('config.php');
 
 // echo "hii";
 
-// echo $name = $_POST['name'];
-
-// $price = $_POST['price'];
-// $dropdown = $_POST['dropdown'];
-// $tags = implode(",", $_POST['fashion']);
-// $description = $_POST['description'];
-
-// $image = $_FILES['image']['name'];
-// $desc = $_FILES['image']['tmp_name'];
-
-// if (move_uploaded_file($image, "productImage/".$desc)) {
-//   $sql = "INSERT INTO product (`name`, `price`, `image`, `long_description`, tags) VALUES ('$name', '$price', '$dropdown', '$tags', '$description', '$image')";
-//   echo $sql;
-//   exit();
-//   mysqli_query($conn, $sql);
-// }
-
-
-
-
-// $errors = array();
-// $message = '';
 $name = $_POST['name'];
 $price = $_POST['price'];
 $dropdown = $_POST['dropdown'];
 $tags = implode(",", $_POST['fashion']);
 $description = $_POST['description'];
+$color = implode(",", $_POST['color']);
 
 $image = $_FILES['image']['name'];
 $desc = $_FILES['image']['tmp_name'];
 
 if (move_uploaded_file($desc, "productImage/".$image)) {
-  $sql = "INSERT INTO product (`name`, `price`, `image`, `long_description`, `tags`, `category`) VALUES ('$name', '$price', '$image', '$description', '$tags', '$dropdown')";
-  // echo $sql;
-  // exit();
-  mysqli_query($conn, $sql);
-  header('LOCATION:products.php');
+$sql = "INSERT INTO product (`name`, `price`, `image`, `long_description`, `tags`, `category`, `color`) VALUES ('$name', '$price', '$image', '$description', '$tags', '$dropdown', '$color')";
+// echo $sql;
+// exit();
+mysqli_query($conn, $sql);
+header('LOCATION:products.php');
 } else {
-  echo "file not uploaded";
+echo "file not uploaded";
 }
-
-
 
 ?>
